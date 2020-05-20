d9aba5cbe91eeb8748568ae859790a0360357749
Juan Gaspar García <gaxdpar@gmail.com>
2020-05-20 11:00:39 +0200

    Initial commit
    IMplemented a basic scene with directional light and basic moveable sprite

---
+++ Player.gd
@@ -0,0 +1,31 @@
+extends KinematicBody
+
+
+onready var spr_body = $SprBody
+
+var move_speed: float = 1 # Pixels per second.
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+func _process(delta: float) -> void:
+	var input_dir = Vector3(
+		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+		0,
+		Input.get_action_strength("move_back") - Input.get_action_strength("move_forward")
+	)
+	
+	if input_dir.length() > 1:
+		input_dir = input_dir.normalized()
+	
+	if input_dir.length() > 0:
+		spr_body.animation = "running"
+		spr_body.flip_h = input_dir.x > 0
+	else:
+		spr_body.animation = "iddle"
+		
+	
+	move_and_slide(input_dir * move_speed)
+	
+	pass
